8b1148b33ed6f481267a03a02d53c1f718bf8ed0
Adrian Alexander <adrianmauricio337@gmail.com>
2021-06-23 09:20:10 -0500

    1.6  feat: filtrar datos de galeria por area

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,57 @@
-function showMoreData () {
-    if(numberPage < 4) {
-        showMore.classList.add('btn--enabled');
-        return;
-    }
-    
-    numberPage += 1;
-    printGallery(numberPage);    
-}
+const addFolder = (itemGallery) => folder.push(itemGallery);
 
-async function printGallery (numberPage = 1) {
-    const portafolio = await fetchData(numberPage);
-    portafolio.map((proyecto) => {                        
-        const proyect = document.createElement('figure');
-        proyect.classList.add('item');
-        proyect.innerHTML = `<img loading="lazy" src="${proyecto.pictureUrl}" alt="imagen de ${proyecto.area}">`;
-        galleryMsonry.appendChild(proyect);        
-    })
-}
+const updateNavbarClass = (navbarItem) => {
+	for (let i = 0; i < navbarItems.length; i++) {
+		navbarItems[i].classList.remove('btn--primary');
+	}
+	navbarItem.classList.add('btn--primary');
+};
 
-printGallery();
+// filtra segun el area elegida
+const filterData = (e) => {
+	galleryMsonry.innerHTML = '';
+	updateNavbarClass(e.target);
+
+	if (e.target.dataset.area === 'all') {
+		printGallery(folder, false);
+		return;
+	}
+
+	let dataFilter = folder.filter(
+		(elemento) => elemento.area === e.target.dataset.area
+	);
+
+	printGallery(dataFilter, false);
+};
+
+// busca mas datos del archivo .json
+const showMoreData = () => {
+	if (numberPage === 4) {
+		showMore.classList.add('btn--enabled');
+		return;
+	}
+
+	numberPage += 1;
+	initData(numberPage);
+};
+
+// Imprime los datos recibidos por parametro
+const printGallery = (data, addtoFolder = true) => {
+	data.map((elemento) => {
+		const element = document.createElement('figure');
+		element.classList.add('item');
+		element.innerHTML = `<img loading="lazy" src="${elemento.pictureUrl}" alt="imagen de ${elemento.area}">`;
+		galleryMsonry.appendChild(element);
+
+		addtoFolder ? addFolder(elemento) : '';
+	});
+};
+
+// obtiene los datos
+const initData = async (numberPage = 1) => {
+	const portafolio = await fetchData(numberPage);
+
+	printGallery(portafolio);
+};
+
+initData();
