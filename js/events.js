d8dd86540da825df38c1910e7cf1217fdfcf2bb3
Adrian Alexander <adrianmauricio337@gmail.com>
2021-06-23 09:19:33 -0500

    1.5  feat: show more images

--- js/events.js
+++ js/events.js
@@ -1,1 +1,5 @@
 showMore.addEventListener('click', showMoreData);
+
+for(let i= 0; i < navbarItems.length; i++){
+    navbarItems[i].addEventListener('click', filterData)
+}
--- js/variables.js
+++ js/variables.js
@@ -1,4 +1,6 @@
 const galleryMsonry = document.getElementById('gallery');
 const showMore = document.getElementById('show-more');
+const navbarItems = document.getElementsByClassName('navbar__item');
 
 let numberPage = 1;
+let folder = []
