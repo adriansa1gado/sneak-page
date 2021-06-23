421e3b14cd0638abaf8bbe93c7bf114095213541
Adrian Alexander <adrianmauricio337@gmail.com>
2021-06-23 13:18:48 -0500

    fix llamada a lo datos de la galeria

--- js/fetchdata.js
+++ js/fetchdata.js
@@ -1,5 +1,5 @@
 function fetchData(numberPage) {
-	return fetch('../data/datadb.json')
+	return fetch('./datadb.json')
 		.then((data) => data.json())
 		.then((data) => data.portafolio[`page-${numberPage}`])
 		.catch(() => console.error('Error get data'));
